9e2b987641011d3f5177adc04dd77e433008c562
Tag <tagadvance+github.com@gmail.com>
2021-08-29 20:40:14 -0600

    Generate Map methods

---
+++ src/com.tagadvance.map/TagsHashMap.java
@@ -0,0 +1,70 @@
+package com.tagadvance.map;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+class TagsHashMap<K, V> implements Map<K, V> {
+    public TagsHashMap() {
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object o) {
+        return false;
+    }
+
+    @Override
+    public V get(Object o) {
+        return null;
+    }
+
+    @Override
+    public V put(K k, V v) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object o) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> map) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
