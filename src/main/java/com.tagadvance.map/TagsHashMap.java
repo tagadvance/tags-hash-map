c9d8beb56f7fc388df00907702329094c8203699
Tag <tagadvance+github.com@gmail.com>
2021-08-29 22:34:07 -0600

    Passing test for #put and #containsKey

--- src/main/java/com.tagadvance.map/TagsHashMap.java
+++ src/main/java/com.tagadvance.map/TagsHashMap.java
@@ -1,11 +1,19 @@
 package com.tagadvance.map;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import com.google.common.base.Preconditions;
+
+import java.util.*;
+import java.util.stream.IntStream;
 
 class TagsHashMap<K, V> implements Map<K, V> {
+    private final List<KeyValuePair> keyValuePairs;
+
     public TagsHashMap() {
+        this.keyValuePairs = new ArrayList<>();
+    }
+
+    public TagsHashMap(int initialSize) {
+        this.keyValuePairs = new ArrayList<>(initialSize);
     }
 
     @Override
@@ -19,8 +27,8 @@ class TagsHashMap<K, V> implements Map<K, V> {
     }
 
     @Override
-    public boolean containsKey(Object o) {
-        return false;
+    public boolean containsKey(Object key) {
+        return this.getIndexOfKey(key).isPresent();
     }
 
     @Override
@@ -34,8 +42,19 @@ class TagsHashMap<K, V> implements Map<K, V> {
     }
 
     @Override
-    public V put(K k, V v) {
-        return null;
+    public V put(K key, V value) {
+        V previousValue = null;
+
+        var i = this.getIndexOfKey(key);
+        if (i.isPresent()) {
+            previousValue = this.keyValuePairs.get(i.getAsInt()).getValue();
+
+            this.keyValuePairs.remove(i);
+        }
+
+        this.keyValuePairs.add(new KeyValuePair(key, value));
+
+        return previousValue;
     }
 
     @Override
@@ -66,5 +85,47 @@ class TagsHashMap<K, V> implements Map<K, V> {
     @Override
     public Set<Entry<K, V>> entrySet() {
         return null;
+    }
+
+    private OptionalInt getIndexOfKey(Object key) {
+        Preconditions.checkNotNull(key, "key must not be null");
+
+        var hashCode = key.hashCode();
+        return IntStream.range(0, this.keyValuePairs.size())
+                .filter(i -> {
+                    var keyValuePair = this.keyValuePairs.get(i);
+                    return hashCode == keyValuePair.getKeyHash() && Objects.equals(key, keyValuePair.getKey());
+                })
+                .findFirst();
+    }
+
+    private class KeyValuePair {
+
+        private final K key;
+        private final V value;
+        private final int keyHash, valueHash;
+
+        public KeyValuePair(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.keyHash = key.hashCode();
+            this.valueHash = value.hashCode();
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public int getKeyHash() {
+            return keyHash;
+        }
+
+        public int getValueHash() {
+            return valueHash;
+        }
     }
 }
